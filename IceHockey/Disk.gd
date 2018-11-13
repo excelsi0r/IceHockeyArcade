a5a5ab48f82e8fbfa2895ad2c11b9499f5b1faae
Nuno Miguel Ladeira Neto <up201406003@feup-b107d10.fe.up.pt>
2018-11-13 15:41:33 +0000

    this solves #2 and solves #1, added speed when hit with stick

--- IceHockey/Disk.gd
+++ IceHockey/Disk.gd
@@ -1,24 +1,45 @@
 extends KinematicBody2D
 
-var INIT_SPEED = 5
+const INIT_SPEED = 5
+const MAX_SPEED = 10
+var curr_speed
+
 var velocity = Vector2()
-var INIT_POS = Vector2()
+var init_pos = Vector2()
 
 func _ready():
-	velocity = Vector2(rand_range(-INIT_SPEED/2, -INIT_SPEED), rand_range(0, INIT_SPEED/2))
-	INIT_POS = global_position
-	
-	pass
-	
-func _process(delta):
-	var collision = move_and_collide( velocity )
-	
-	if collision:
-		velocity = velocity.bounce(collision.normal)
+	init_pos = global_position
+	reset()
 	pass
 	
 func reset():
-	global_position = INIT_POS
-	velocity = Vector2(rand_range(-INIT_SPEED/2, -INIT_SPEED), rand_range(0, INIT_SPEED/2))
+	global_position = init_pos
+	var angle = rand_range(0, PI * 2)
+	velocity = calculate_velocity(angle, INIT_SPEED)
+	curr_speed = INIT_SPEED
 	pass
+	
+func _process(delta):
+	
+	if curr_speed > INIT_SPEED:
+		curr_speed -= 5*delta
+	
+	var collision = move_and_collide(velocity)
+	
+	if collision != null && collision.collider.name == "Stick" && (Input.is_key_pressed(KEY_S) || Input.is_key_pressed(KEY_W) || Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_right")):
+		curr_speed = MAX_SPEED
+	
+	if collision:
+		velocity = velocity.bounce(collision.normal)
+		
+	velocity = calculate_velocity(velocity.angle(), curr_speed)
+		
+	pass
+	
+func calculate_velocity(angle, speed):
+	var x = speed * cos(angle)
+	var y = speed * sin(angle)
+	return Vector2(x, y)
+	
+
 	
